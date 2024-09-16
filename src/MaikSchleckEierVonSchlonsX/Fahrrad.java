8fe82ea340a2c88e28c599185f172a7d70642d39
stein202 <steinfredl5@gmail.com>
2024-09-16 20:39:56 +0200

    😎

--- src/MaikSchleckEierVonSchlonsX/Fahrrad.java
+++ src/MaikSchleckEierVonSchlonsX/Fahrrad.java
@@ -3,7 +3,6 @@ package src.MaikSchleckEierVonSchlonsX;
 public class Fahrrad extends Fahrzeug{
     private double rahmenhoehe;
 
-
     public Fahrrad(String a, double b, double c, double d) {
         super(a, b, c);
         this.rahmenhoehe = d;
